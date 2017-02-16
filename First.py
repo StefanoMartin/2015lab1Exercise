7e32ac3d31d14f253a1e1bcb3a50388539aa1a0e
Stefano Martin <Stefano Martin>
2017-02-16 19:23:42 +0100

    Test

---
+++ First.py
@@ -0,0 +1,13 @@
+import sys
+import scipy
+import numpy
+import matplotlib
+import pandas
+import sklearn
+
+print("Python: {}".format(sys.version))
+print("Scipy: {}".format(scipy.__version__))
+print("Numpy: {}".format(numpy.__version__))
+print("Matplotlib: {}".format(matplotlib.__version__))
+print("Pandas: {}".format(pandas.__version__))
+print("Sklearn: {}".format(sklearn.__version__))
